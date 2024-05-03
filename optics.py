55a8aa4cf826b2a140c704672ba19129343b8645
Hitoshi Wako <Hitoshi.Wako@gmail.com>
2024-05-04 00:10:58 +0900

    Merge CIE

--- optics.py
+++ optics.py
@@ -1,6 +1,13 @@
 import numpy as np
 import pandas as pd
 
+df_xyz = pd.read_csv('CIE_DATASETS/CIE_xyz_1931_2deg.csv',names=['X','Y','Z'],index_col=0)
+df_illum = pd.read_csv('CIE_DATASETS/CIE_std_illum_D65.csv',names=['D65'],index_col=0)
+df_xyz = df_xyz.loc[360:780]
+df_illum = df_illum.loc[360:780]
+(Xw,Yw,Zw) = df_xyz.T.dot(df_illum['D65'])
+(Xn,Yn,Zn)=(Xw/Yw*100,Yw/Yw*100,Zw/Yw*100)
+
 def admittance(angle,n,n0=np.array([1.0])):
     x = np.sqrt(np.power(n,2) - np.power(n0*np.sin(angle),2))
     return np.array([x,np.power(n,2)/x])
@@ -21,3 +28,12 @@ def reflectance(angle,ns,ds=[],df=pd.DataFrame(),n0=np.array([1.0])):
     C = M[1,0]+M[1,1]*etas
     r = np.power(np.abs((eta0*B-C)/(eta0*B+C)),2)
     return r
+
+def spectrum_xyz(spec):
+    return df_xyz.mul(df_illum['D65'],axis=0).T.dot(spec)/Yw*100
+
+def xyz_lab(X,Y,Z):
+    def f(t):
+        return t**(1/3) if t > (6/29)**3 else 1/3*(29/6)**2*t+4/29
+    fy = f(Y/Yn)
+    return 116*fy-16,500*(f(X/Xn)-fy),200*(fy-f(Z/Zn))
